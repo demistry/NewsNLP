da40c3f2488ce18b1d5599ab9f1d6372240c3526
Demistry <dave11ilen@gmail.com>
2019-04-15 15:09:40 +0100

    Initial Commit

---
+++ NewsNLP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsNLP
+//
+//  Created by David Ilenwabor on 15/04/2019.
+//  Copyright © 2019 Demistry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
